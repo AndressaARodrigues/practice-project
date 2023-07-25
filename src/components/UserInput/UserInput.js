82f20bf790955c7cbd7fe9e24a0d0bb73365ccb2
Andressa Rodrigues <56194834+AndressaARodrigues@users.noreply.github.com>
2023-07-25 00:04:20 -0300

    Adding CSS Modules

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,97 +1,101 @@
-import { useState } from "react";
+import { useState } from 'react';
+import classes from './UserInput.module.css';
 
 const initialUserInput = {
-    'current-savings': 10000,
-    'yearly-contribution': 1200,
-    'expected-return': 7,
-    'duration': 10,
+  'current-savings': 10000,
+  'yearly-contribution': 1200,
+  'expected-return': 7,
+  duration: 10,
 };
 
 const UserInput = (props) => {
-    const [userInput, setUserInput] = useState(initialUserInput);
+  const [userInput, setUserInput] = useState(initialUserInput);
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        
-        props.onCalculate(userInput);
-    };
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-    const resetHandler = () => {
-        setUserInput(initialUserInput);
-    };
+    props.onCalculate(userInput);
+  };
 
-    const inputchangeHandler = (input, value) => {
-        setUserInput((prevInput) => {
-            return {
-             ...prevInput,
-             [input]: +value,
-            };
-        });
-    };
+  const resetHandler = () => {
+    setUserInput(initialUserInput);
+  };
 
-    return (
-      <form onSubmit={submitHandler} className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input 
-                onChange={(event) => 
-                 inputchangeHandler('current-savings', event.target.value)
-                } 
-                value={userInput['current-savings']}
-                type="number" 
-                id="current-savings" 
-            />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input 
-                onChange={(event) => 
-                inputchangeHandler('yearly-contribution', event.target.value)
-                } 
-                value={userInput['yearly-contribution']}
-                type="number" 
-                id="yearly-contribution" 
-            />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input 
-                onChange={(event) => 
-                inputchangeHandler('expected-return', event.target.value)
-                } 
-                value={userInput['expected-return']}
-                type="number" 
-                id="expected-return" 
-            />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-            <input 
-                onChange={(event) => 
-                inputchangeHandler('duration', event.target.value)
-                } 
-                value={userInput['duration']}
-                type="number" 
-                id="duration" 
-            />
-          </p>
-        </div>
-        <p className="actions">
-          <button onClick={resetHandler} type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
+  const inputChangeHandler = (input, value) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input]: +value,
+      };
+    });
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <div className={classes['input-group']}>
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler('current-savings', event.target.value)
+            }
+            value={userInput['current-savings']}
+            type="number"
+            id="current-savings"
+          />
         </p>
-      </form>
-    );
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler('yearly-contribution', event.target.value)
+            }
+            value={userInput['yearly-contribution']}
+            type="number"
+            id="yearly-contribution"
+          />
+        </p>
+      </div>
+      <div className={classes['input-group']}>
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler('expected-return', event.target.value)
+            }
+            value={userInput['expected-return']}
+            type="number"
+            id="expected-return"
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler('duration', event.target.value)
+            }
+            value={userInput['duration']}
+            type="number"
+            id="duration"
+          />
+        </p>
+      </div>
+      <p className={classes.actions}>
+        <button
+          onClick={resetHandler}
+          type="reset"
+          className={classes.buttonAlt}
+        >
+          Reset
+        </button>
+        <button type="submit" className={classes.button}>
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
 };
 
 export default UserInput;
